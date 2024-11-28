1d82ac8dbbfce51854342712cbd308d5c7044e6a
Yaniv Efremov <yanivefremov@Yanivs-MacBook-Pro.local>
2024-11-28 22:58:18 +0200

    added worker communication with sqs and s3

--- src/main/java/com/edu/Main.java
+++ src/main/java/com/edu/Main.java
@@ -1,19 +1,68 @@
 package com.edu;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SqsException;
 
 public class Main {
     public static void main(String[] args) {
-        //print current dir
-        System.out.println(System.getProperty("user.dir"));
-        String action = "toHTML";
-        String fileURL = "http://scheinerman.net/judaism/pesach/haggadah.pdf"; // Replace with your URL
-        String saveDir = "midWork";
+        // Create the SQS client
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
 
-        try {
-            PdfWorker.work(action ,fileURL, saveDir);
-        } catch (Exception e) {
-            e.printStackTrace();
+        String queueUrl = "https://sqs.us-east-1.amazonaws.com/975050155862/manager-worker"; // Replace with your queue URL
+
+        while (true) {
+            try {
+                // Receive messages from the queue
+                ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
+                        .queueUrl(queueUrl)
+                        .maxNumberOfMessages(1)
+                        .waitTimeSeconds(10) // Polling wait time for long polling
+                        .build();
+                ReceiveMessageResponse receiveResponse = sqsClient.receiveMessage(receiveRequest);
+                //print the message
+                System.out.println("Received messages: " + receiveResponse.messages().size());
+                // Exit if we get kill signal
+                
+                // Process each received message
+                for (Message message : receiveResponse.messages()) {
+                    try {
+                        System.out.println("Processing message: " + message.body());
+                        // Parse the message body
+                        String[] messageParts = message.body().split(",");
+                        String action = messageParts[0];
+                        String fileURL = messageParts[1];
+                        String saveDir = "midWork"; // Temporary save directory
+
+                        // Call the PdfWorker
+                        PdfWorker.work(action, fileURL, saveDir);
+
+                        // Delete the message from the queue
+                        DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder()
+                                .queueUrl(queueUrl)
+                                .receiptHandle(message.receiptHandle())
+                                .build();
+                        sqsClient.deleteMessage(deleteRequest);
+
+                        System.out.println("Processed and deleted message: " + message.body());
+                    } catch (Exception e) {
+                        System.err.println("Error processing message: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            } catch (SqsException e) {
+                System.err.println("SQS Error: " + e.awsErrorDetails().errorMessage());
+                System.exit(1); // Exit if there's a fatal SQS issue
+            } catch (Exception e) {
+                System.err.println("Unexpected error: " + e.getMessage());
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/com/edu/PdfWorker.java
+++ src/main/java/com/edu/PdfWorker.java
@@ -15,9 +15,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileWriter;
 
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
 public class PdfWorker {
 
     public static void work(String Action, String fileUrl, String saveDir) throws Exception {
+        S3Client s3Client = S3Client.create();
+
         try {
             // Download the PDF file
             URL url = new URL(fileUrl);
@@ -39,6 +45,8 @@ public class PdfWorker {
             return;
         }
 
+        File completedFile = null; // To track the generated file for upload
+
         // Perform the specified action on the first page
         switch (Action) {
             case "toImage":
@@ -48,10 +56,10 @@ public class PdfWorker {
 
                     // Render the first page as an image
                     BufferedImage bim = pdfRenderer.renderImageWithDPI(0, 300, ImageType.RGB);
-                    File outputFile = new File(saveDir + "-page-1.png");
-                    ImageIO.write(bim, "png", outputFile);
+                    completedFile = new File(saveDir + "-page-1.png");
+                    ImageIO.write(bim, "png", completedFile);
 
-                    System.out.println("Saved first page as image: " + outputFile.getAbsolutePath());
+                    System.out.println("Saved first page as image: " + completedFile.getAbsolutePath());
                     document.close();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -67,12 +75,12 @@ public class PdfWorker {
 
                     // Extract text from the first page
                     String text = textStripper.getText(document);
-                    File textFile = new File(saveDir + "-page-1.txt");
-                    try (FileWriter writer = new FileWriter(textFile)) {
+                    completedFile = new File(saveDir + "-page-1.txt");
+                    try (FileWriter writer = new FileWriter(completedFile)) {
                         writer.write(text);
                     }
 
-                    System.out.println("Saved text of first page: " + textFile.getAbsolutePath());
+                    System.out.println("Saved text of first page: " + completedFile.getAbsolutePath());
                     document.close();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -97,12 +105,12 @@ public class PdfWorker {
 
                     htmlContent.append("</div></body></html>");
 
-                    File htmlFile = new File(saveDir + "-page-1.html");
-                    try (FileWriter writer = new FileWriter(htmlFile)) {
+                    completedFile = new File(saveDir + "-page-1.html");
+                    try (FileWriter writer = new FileWriter(completedFile)) {
                         writer.write(htmlContent.toString());
                     }
 
-                    System.out.println("Saved HTML of first page: " + htmlFile.getAbsolutePath());
+                    System.out.println("Saved HTML of first page: " + completedFile.getAbsolutePath());
                     document.close();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -111,6 +119,29 @@ public class PdfWorker {
 
             default:
                 System.out.println("Unknown action: " + Action);
+                return;
         }
+
+        // Upload the completed file to S3
+        if (completedFile != null && completedFile.exists()) {
+            try {
+                PutObjectRequest putRequest = PutObjectRequest.builder()
+                        .bucket("resultbucket-aws1")
+                        .key(completedFile.getName())
+                        .build();
+
+                s3Client.putObject(putRequest, completedFile.toPath());
+                System.out.println("Uploaded file to S3: " + completedFile.getName());
+            } catch (S3Exception e) {
+                System.err.println("S3 Error: " + e.awsErrorDetails().errorMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No file was generated to upload.");
+        }
+
+        // Close the S3 client
+        s3Client.close();
     }
 }
